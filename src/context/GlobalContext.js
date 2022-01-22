6830a495a68ab11877f47e2877b18920942af812
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-01-21 19:22:02 -0500

    list storage update

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -3,8 +3,43 @@ import { createContext, useReducer, useEffect } from 'react';
 import GlobalReducer from './GlobalReducer';
 
 // initial state
+const newList = [
+  {
+    id: 'time',
+    name: 'Time',
+    status: false,
+  },
+  {
+    id: 'weather',
+    name: 'Weather',
+    status: false,
+  },
+  {
+    id: 'news',
+    name: 'News',
+    status: false,
+  },
+  {
+    id: 'covid',
+    name: 'Covid Info',
+    status: false,
+  },
+  {
+    id: 'blackScreen',
+    name: 'Black Screen',
+    status: false,
+  },
+  { id: 'calculator', name: 'Calculator', status: false },
+];
 
-const list = JSON.parse(localStorage.getItem('list')) || null;
+let list = JSON.parse(localStorage.getItem('list')) || null;
+
+if (list) {
+  if (list.length >= newList.length) {
+    list = newList;
+    console.log(true);
+  }
+}
 
 const initialState = {
   location: JSON.parse(localStorage.getItem('location')) || null,
