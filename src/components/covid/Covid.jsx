fc090a281c87dfee5a8ff6234acbcc3620198906
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-01-23 01:25:19 -0500

    added more colors to black screen widget

--- src/components/covid/Covid.jsx
+++ src/components/covid/Covid.jsx
@@ -6,6 +6,7 @@ import { countries } from './countries';
 import ChartDaily from './ChartDaily';
 import CloseWidget from '../closeWidget/CloseWidget';
 import Draggable from 'react-draggable';
+import Loading from '../loading/Loading';
 
 const Covid = () => {
   const [covidData, setCovidData] = useState();
@@ -13,6 +14,7 @@ const Covid = () => {
   const [activeTab, setActiveTab] = useState('cases');
   const [selCountry, setSelCountry] = useState();
   const [isActiveChart, setIsActiveChart] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const url1 = 'https://disease.sh/v3/covid-19/countries';
   const url2 = 'https://disease.sh/v3/covid-19/historical?lastdays=30';
@@ -22,8 +24,10 @@ const Covid = () => {
       const res = await axios.get(url);
       if (a === 'covid') {
         setCovidData(res.data);
+        setLoading(false);
       } else {
         setDailyData(res.data);
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
@@ -78,100 +82,106 @@ const Covid = () => {
   return (
     <Draggable handle="strong">
       <div className="chartWrapper box no-cursor">
-        <strong className="cursor" style={{ width: 100 + '%' }}></strong>
-        <CloseWidget id="covid" />
-        <button
-          onClick={() => setIsActiveChart(!isActiveChart)}
-          className={
-            !isActiveChart ? 'chartTotalsBtn activeTBtn' : 'chartTotalsBtn'
-          }
-        >
-          Totals
-        </button>
-        <button
-          onClick={() => {
-            setIsActiveChart(!isActiveChart);
-            handleClick('cases');
-          }}
-          className={
-            isActiveChart ? 'chartDailyBtn activeTBtn' : 'chartDailyBtn'
-          }
-        >
-          Daily
-        </button>
-        {isActiveChart ? (
-          <div className="chartTotal">
-            <h2 className="chartTitle">
-              Daily Covid-19 Cases - {selCountry[0]?.country}
-            </h2>
-            <div className="selCountry">
-              <label htmlFor="country">Countries: </label>
-              <select
-                name="country"
-                onChange={(e) => handleSelect(e.target.value)}
-                defaultValue="USA"
-              >
-                {countries.map((item, index) => {
-                  return (
-                    <option value={item} key={index}>
-                      {item}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            {dailyData &&
-              dailData.map((item, index) => {
-                return (
-                  <button
-                    onClick={() => handleClick(item.dataKey)}
-                    className={
-                      activeTab === item.dataKey
-                        ? `${item.dataKey}Btn activeTotalBtn`
-                        : `${item.dataKey}Btn`
-                    }
-                    key={index}
-                  >
-                    {item.name}
-                  </button>
-                );
-              })}
-
-            <ChartDaily dData={selCountry} activeTab={activeTab} />
-          </div>
+        {loading ? (
+          <Loading width={'50px'} height={'50px'} />
         ) : (
-          <div className="chartTotal">
-            <h2 className="chartTitle">
-              Total Covid-19 Cases (top 10 countries)
-            </h2>
-            {covidData &&
-              charData.map((item, index) => {
-                return (
-                  <div key={item.dataKey}>
-                    <button
-                      onClick={() => handleClick(item.dataKey)}
-                      className={
-                        activeTab === item.dataKey
-                          ? `${item.dataKey}Btn activeTotalBtn`
-                          : `${item.dataKey}Btn`
-                      }
-                    >
-                      {' '}
-                      {item.name}
-                    </button>
-                    <div
-                      className={
-                        activeTab === item.dataKey
-                          ? 'chartTab activeChart'
-                          : 'chartTab'
-                      }
-                    >
-                      <CovidChart covidData={covidData} item={item} />
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
+          <>
+            <strong className="cursor" style={{ width: 100 + '%' }}></strong>
+            <CloseWidget id="covid" />
+            <button
+              onClick={() => setIsActiveChart(!isActiveChart)}
+              className={
+                !isActiveChart ? 'chartTotalsBtn activeTBtn' : 'chartTotalsBtn'
+              }
+            >
+              Totals
+            </button>
+            <button
+              onClick={() => {
+                setIsActiveChart(!isActiveChart);
+                handleClick('cases');
+              }}
+              className={
+                isActiveChart ? 'chartDailyBtn activeTBtn' : 'chartDailyBtn'
+              }
+            >
+              Daily
+            </button>
+            {isActiveChart ? (
+              <div className="chartTotal">
+                <h2 className="chartTitle">
+                  Daily Covid-19 Cases - {selCountry[0]?.country}
+                </h2>
+                <div className="selCountry">
+                  <label htmlFor="country">Countries: </label>
+                  <select
+                    name="country"
+                    onChange={(e) => handleSelect(e.target.value)}
+                    defaultValue="USA"
+                  >
+                    {countries.map((item, index) => {
+                      return (
+                        <option value={item} key={index}>
+                          {item}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                {dailyData &&
+                  dailData.map((item, index) => {
+                    return (
+                      <button
+                        onClick={() => handleClick(item.dataKey)}
+                        className={
+                          activeTab === item.dataKey
+                            ? `${item.dataKey}Btn activeTotalBtn`
+                            : `${item.dataKey}Btn`
+                        }
+                        key={index}
+                      >
+                        {item.name}
+                      </button>
+                    );
+                  })}
+
+                <ChartDaily dData={selCountry} activeTab={activeTab} />
+              </div>
+            ) : (
+              <div className="chartTotal">
+                <h2 className="chartTitle">
+                  Total Covid-19 Cases (top 10 countries)
+                </h2>
+                {covidData &&
+                  charData.map((item, index) => {
+                    return (
+                      <div key={item.dataKey}>
+                        <button
+                          onClick={() => handleClick(item.dataKey)}
+                          className={
+                            activeTab === item.dataKey
+                              ? `${item.dataKey}Btn activeTotalBtn`
+                              : `${item.dataKey}Btn`
+                          }
+                        >
+                          {' '}
+                          {item.name}
+                        </button>
+                        <div
+                          className={
+                            activeTab === item.dataKey
+                              ? 'chartTab activeChart'
+                              : 'chartTab'
+                          }
+                        >
+                          <CovidChart covidData={covidData} item={item} />
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            )}
+          </>
         )}
       </div>
     </Draggable>
