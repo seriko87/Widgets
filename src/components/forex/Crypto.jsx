bf03d3ff30e8f7cc2e626123df0c7a5f008d8be8
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-01-24 15:54:05 -0500

    update

--- src/components/forex/Crypto.jsx
+++ src/components/forex/Crypto.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './crypto.css';
 
-const Crypto = () => {
-  return <div></div>;
+const Crypto = ({ crypto, cryptoPrice }) => {
+  const [priChange, setPriChange] = useState();
+  const { label, icon, symbol } = crypto;
+  const price = cryptoPrice.price;
+  const priceChange = cryptoPrice.pricePercentChange.change24h;
+
+  useEffect(() => {
+    priceChange > 0 ? setPriChange(true) : setPriChange(false);
+  }, [priceChange]);
+
+  let str = price.toString().split('');
+  let newPrice = '';
+  console.log(str);
+
+  str.map((item, index) => {
+    if (item === '.') {
+    }
+  });
+  return (
+    <div className="forexTitle">
+      <div className="forexIcon">
+        <img src={icon} alt={label} />
+        <span>{label + '-' + symbol}</span>
+      </div>
+      <div className="forexPrice">
+        <div className="cryptoPrice">
+          <span>{Math.fround(price)}</span>
+          <span className={priChange ? 'priceUp' : 'priceDown'}>
+            {priceChange}
+          </span>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Crypto;
--- src/components/forex/Forex.jsx
+++ src/components/forex/Forex.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import TextField from '@mui/material/TextField';
+
 import Autocomplete from '@mui/material/Autocomplete';
 import './forex.css';
 import Crypto from './Crypto';
@@ -12,17 +12,8 @@ const Forex = () => {
   const RAPID_KEY = process.env.REACT_APP_RAPID_API;
 
   const token =
-    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5EVXhNRGhHT0VReE56STVOelJCTTBJM1FrUTVOa0l4TWtRd1FrSTJSalJFTVRaR1F6QTBOZyJ9.eyJpc3MiOiJodHRwczovL2F1dGguYnJhdmVuZXdjb2luLmNvbS8iLCJzdWIiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWUBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGkuYnJhdmVuZXdjb2luLmNvbSIsImlhdCI6MTY0Mjg3Nzk2MCwiZXhwIjoxNjQyOTY0MzYwLCJhenAiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWSIsInNjb3BlIjoicmVhZDppbmRleC10aWNrZXIgcmVhZDpyYW5raW5nIHJlYWQ6bXdhIHJlYWQ6Z3dhIHJlYWQ6YWdncmVnYXRlcyByZWFkOm1hcmtldCByZWFkOmFzc2V0IHJlYWQ6b2hsY3YgcmVhZDptYXJrZXQtY2FwIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.LUc2seQB-vfTBZF9QA5sOcL1WX5J8bJS-0NPH4mXyyLfO5xaAx4Rx75EGaqzqejRH72wDBfyR3pAtjPViRZQhX5OfuooHgqKXgZ3eIfcAYGGhrZ3IPVor1lI3P7xEPnEY17Mbi9WKPE0HMNvjlbfzsj3u9xOet14PdgG0hbbrg7H9jHHlRvhbVWrDO_ODLqKTO3fq8etlV3z_YIq_FVGbQobmXncXAojDQ_hNxVeH1au8lQSNOpq3KWr5etJmpLyyFNvEoyn97Q9jpOkDWxwyHm9UDVLrQ7hmbX9UAB3AI4I2gEZmywKnbmPGotBqwc5xtuKYT0JkCqWlYI6-CY50g';
-  //   const get_price = {
-  //     method: 'GET',
-  //     url: 'https://bravenewcoin.p.rapidapi.com/market-cap',
-  //     params: { assetId: crypto.id },
-  //     headers: {
-  //       authorization: `Bearer ${token}`,
-  //       'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
-  //       'x-rapidapi-key': RAPID_KEY,
-  //     },
-  //   };
+    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5EVXhNRGhHT0VReE56STVOelJCTTBJM1FrUTVOa0l4TWtRd1FrSTJSalJFTVRaR1F6QTBOZyJ9.eyJpc3MiOiJodHRwczovL2F1dGguYnJhdmVuZXdjb2luLmNvbS8iLCJzdWIiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWUBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGkuYnJhdmVuZXdjb2luLmNvbSIsImlhdCI6MTY0Mjk1NTU5MSwiZXhwIjoxNjQzMDQxOTkxLCJhenAiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWSIsInNjb3BlIjoicmVhZDppbmRleC10aWNrZXIgcmVhZDpyYW5raW5nIHJlYWQ6bXdhIHJlYWQ6Z3dhIHJlYWQ6YWdncmVnYXRlcyByZWFkOm1hcmtldCByZWFkOmFzc2V0IHJlYWQ6b2hsY3YgcmVhZDptYXJrZXQtY2FwIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.RU5fDsNvs2v3_i6WLDerLo73BfVyJ3si3fGhpDxRVs0cpePeiUSyIGa7SmLdQ3aDlueZsm6RI8h7m_jFnGIbuVm7S_8LQB-lucP5iLWmANThRIhiIlmZpuvLqp_vMI20BUFw7rzpgCkiVMw4rHT9P25gDlto3pXnGpeNaihvCXKFbshyEbi9No4nF1LFrhdSQmYintQYiQnHMArHuvZfBWUB6Z076KYfdA2u28jjepJs5Z1VBacohpp4bOABiZx8Tg_JgXBYfaQkj_zCUwL1CqB2u73Zw6emlBznIlRg1WRQGUFe44yugPHCBmGQbh0BpzHj3XQIJ07cVeA3Vc8CXA';
+
   const get_token = {
     method: 'POST',
     url: 'https://bravenewcoin.p.rapidapi.com/oauth/token',
@@ -37,21 +28,12 @@ const Forex = () => {
       grant_type: 'client_credentials',
     },
   };
-  const get_asset = {
-    method: 'GET',
-    url: 'https://bravenewcoin.p.rapidapi.com/asset',
-    params: { status: 'ACTIVE', type: 'CRYPTO' },
-    headers: {
-      'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
-      'x-rapidapi-key': 'f3c43aa4bbmshb551f3831774a99p12cdf3jsn2618791e08b9',
-    },
-  };
 
   const handeChange = async (id) => {
     const get_price = {
       method: 'GET',
       url: 'https://bravenewcoin.p.rapidapi.com/market-cap',
-      params: { assetId: id },
+      params: { assetId: id, percentChange: true },
       headers: {
         authorization: `Bearer ${token}`,
         'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
@@ -60,13 +42,11 @@ const Forex = () => {
     };
     try {
       const res = await axios(get_price);
-      setCryptoPrice(res.data);
+      setCryptoPrice(res.data.content[0]);
     } catch (error) {
       console.log('API call error:', error);
     }
   };
-
-  console.log(cryptoPrice);
 
   return (
     <div className="forexContainer">
@@ -93,20 +73,13 @@ const Forex = () => {
             />
           </div>
         )}
-        // renderInput={(params) => (
-        //   <TextField {...params} label="Search Crypto" />
-        // )}
         onChange={(event, item) => {
           setCrypto(item);
+
           handeChange(item.id);
         }}
       />
-
-      <div>
-        {crypto.label} <br />
-        {crypto.symbol} <br />
-        {crypto.url} <br />
-      </div>
+      {cryptoPrice && <Crypto cryptoPrice={cryptoPrice} crypto={crypto} />}
     </div>
   );
 };
