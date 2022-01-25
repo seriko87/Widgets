0e7c18107ae2d494156a5b7c1b0467bcab868167
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-01-24 20:26:53 -0500

    update

--- src/components/forex/Crypto.jsx
+++ src/components/forex/Crypto.jsx
@@ -1,39 +1,102 @@
 import React, { useEffect, useState } from 'react';
 import './crypto.css';
+import axios from 'axios';
+import { formatPrice, numberFormatter } from '../../functions/functions';
 
-const Crypto = ({ crypto, cryptoPrice }) => {
+const Crypto = ({ crypto, token }) => {
   const [priChange, setPriChange] = useState();
+  const [cryptoPrice, setCryptoPrice] = useState();
+  const [price, setPrice] = useState();
   const { label, icon, symbol } = crypto;
-  const price = cryptoPrice.price;
-  const priceChange = cryptoPrice.pricePercentChange.change24h;
+  const [formattedPrice, setFormattedPrice] = useState(0);
+  const [fontSize, setFontSize] = useState(36);
+  const RAPID_KEY = process.env.REACT_APP_RAPID_API;
+  let priceChange = cryptoPrice?.pricePercentChange.change24h;
 
   useEffect(() => {
     priceChange > 0 ? setPriChange(true) : setPriChange(false);
   }, [priceChange]);
 
-  let str = price.toString().split('');
-  let newPrice = '';
-  console.log(str);
-
-  str.map((item, index) => {
-    if (item === '.') {
+  useEffect(() => {
+    if (price) {
+      price > 1
+        ? setFormattedPrice(price.toFixed(2))
+        : setFormattedPrice(formatPrice(price));
     }
-  });
+  }, [price]);
+
+  const getCryptoPrice = async (id) => {
+    const get_price = {
+      method: 'GET',
+      url: 'https://bravenewcoin.p.rapidapi.com/market-cap',
+      params: { assetId: id, percentChange: true },
+      headers: {
+        authorization: `Bearer ${token}`,
+        'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
+        'x-rapidapi-key': RAPID_KEY,
+      },
+    };
+    try {
+      const res = await axios(get_price);
+      setCryptoPrice(res.data.content[0]);
+      setPrice(res.data.content[0].price);
+    } catch (error) {
+      console.log('API call error:', error);
+    }
+  };
+  useEffect(() => {
+    getCryptoPrice(crypto.id);
+  }, [crypto.id]);
+
+  useEffect(() => {
+    let curLeng = formattedPrice.length;
+
+    if (curLeng > 10) {
+      setFontSize(25);
+    } else {
+      setFontSize(36);
+    }
+  }, [formattedPrice]);
+
   return (
-    <div className="forexTitle">
-      <div className="forexIcon">
+    <div className="cryptoContainer">
+      <div className="cryptoIconWrap">
         <img src={icon} alt={label} />
         <span>{label + '-' + symbol}</span>
       </div>
-      <div className="forexPrice">
-        <div className="cryptoPrice">
-          <span>{Math.fround(price)}</span>
-          <span className={priChange ? 'priceUp' : 'priceDown'}>
-            {priceChange}
-          </span>
+      {cryptoPrice && (
+        <div className="cryptoInfoContainer">
+          <div className="cryptoPriceWrap">
+            <span className="cryptoPrice">
+              <span style={{ fontSize: fontSize + 'px' }}>
+                {formattedPrice}
+              </span>
+              <span style={{ fontSize: 20 + 'px' }}> USD</span>{' '}
+              <span className={priChange ? 'priceUp' : 'priceDown'}>
+                ({priceChange}%)
+              </span>
+            </span>
+          </div>
+          <div className="cryptoOtherWrap">
+            <div className="cryptoOtherInfo">
+              <span className="cryptoRank">{cryptoPrice?.marketCapRank}</span>
+              <span className="cryptoRankTitle">Rank</span>
+            </div>
+            <div className="cryptoOtherCap">
+              <span className="cryptoRank">
+                ${numberFormatter(cryptoPrice?.marketCap)}
+              </span>
+              <span className="cryptoRankTitle">Market Cap</span>
+            </div>
+            <div className="cryptoOtherCap">
+              <span className="cryptoRank">
+                {numberFormatter(cryptoPrice?.freeFloatSupply)}
+              </span>
+              <span className="cryptoRankTitle">Supply</span>
+            </div>
+          </div>
         </div>
-        <div></div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/forex/Forex.jsx
+++ src/components/forex/Forex.jsx
@@ -8,45 +8,36 @@ import { forexData } from './forexData';
 
 const Forex = () => {
   const [crypto, setCrypto] = useState('');
-  const [cryptoPrice, setCryptoPrice] = useState();
+  const [token, setToken] = useState('');
+
   const RAPID_KEY = process.env.REACT_APP_RAPID_API;
 
-  const token =
-    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5EVXhNRGhHT0VReE56STVOelJCTTBJM1FrUTVOa0l4TWtRd1FrSTJSalJFTVRaR1F6QTBOZyJ9.eyJpc3MiOiJodHRwczovL2F1dGguYnJhdmVuZXdjb2luLmNvbS8iLCJzdWIiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWUBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGkuYnJhdmVuZXdjb2luLmNvbSIsImlhdCI6MTY0Mjk1NTU5MSwiZXhwIjoxNjQzMDQxOTkxLCJhenAiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWSIsInNjb3BlIjoicmVhZDppbmRleC10aWNrZXIgcmVhZDpyYW5raW5nIHJlYWQ6bXdhIHJlYWQ6Z3dhIHJlYWQ6YWdncmVnYXRlcyByZWFkOm1hcmtldCByZWFkOmFzc2V0IHJlYWQ6b2hsY3YgcmVhZDptYXJrZXQtY2FwIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.RU5fDsNvs2v3_i6WLDerLo73BfVyJ3si3fGhpDxRVs0cpePeiUSyIGa7SmLdQ3aDlueZsm6RI8h7m_jFnGIbuVm7S_8LQB-lucP5iLWmANThRIhiIlmZpuvLqp_vMI20BUFw7rzpgCkiVMw4rHT9P25gDlto3pXnGpeNaihvCXKFbshyEbi9No4nF1LFrhdSQmYintQYiQnHMArHuvZfBWUB6Z076KYfdA2u28jjepJs5Z1VBacohpp4bOABiZx8Tg_JgXBYfaQkj_zCUwL1CqB2u73Zw6emlBznIlRg1WRQGUFe44yugPHCBmGQbh0BpzHj3XQIJ07cVeA3Vc8CXA';
-
-  const get_token = {
-    method: 'POST',
-    url: 'https://bravenewcoin.p.rapidapi.com/oauth/token',
-    headers: {
-      'content-type': 'application/json',
-      'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
-      'x-rapidapi-key': RAPID_KEY,
-    },
-    data: {
-      audience: 'https://api.bravenewcoin.com',
-      client_id: 'oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY',
-      grant_type: 'client_credentials',
-    },
-  };
-
-  const handeChange = async (id) => {
-    const get_price = {
-      method: 'GET',
-      url: 'https://bravenewcoin.p.rapidapi.com/market-cap',
-      params: { assetId: id, percentChange: true },
+  const getToken = async () => {
+    const get_token = {
+      method: 'POST',
+      url: 'https://bravenewcoin.p.rapidapi.com/oauth/token',
       headers: {
-        authorization: `Bearer ${token}`,
+        'content-type': 'application/json',
         'x-rapidapi-host': 'bravenewcoin.p.rapidapi.com',
         'x-rapidapi-key': RAPID_KEY,
       },
+      data: {
+        audience: 'https://api.bravenewcoin.com',
+        client_id: 'oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY',
+        grant_type: 'client_credentials',
+      },
     };
     try {
-      const res = await axios(get_price);
-      setCryptoPrice(res.data.content[0]);
+      const res = await axios(get_token);
+      setToken(res.data.access_token);
     } catch (error) {
       console.log('API call error:', error);
     }
   };
+
+  useEffect(() => {
+    getToken();
+  }, []);
 
   return (
     <div className="forexContainer">
@@ -75,11 +66,9 @@ const Forex = () => {
         )}
         onChange={(event, item) => {
           setCrypto(item);
-
-          handeChange(item.id);
         }}
       />
-      {cryptoPrice && <Crypto cryptoPrice={cryptoPrice} crypto={crypto} />}
+      {crypto && <Crypto token={token} crypto={crypto} />}
     </div>
   );
 };
