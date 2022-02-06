b1552884d3d1cdcb8f70ec18f9130e39da004128
Sera Ashyrv <58387052+seriko87@users.noreply.github.com>
2022-02-06 18:43:57 -0500

    bug fixes and improvement

--- src/widgets/imgWidget/ImgWidget.jsx
+++ src/widgets/imgWidget/ImgWidget.jsx
@@ -6,6 +6,7 @@ import './imgWidget.css';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 import Loading from '../../components/loading/Loading';
+import DownloadIcon from '@mui/icons-material/Download';
 const cat = ['Nature', 'Tigers', 'Ocean', 'Dogs', 'Cats'];
 
 const ImgWidget = () => {
@@ -81,11 +82,11 @@ const ImgWidget = () => {
       <div className="imgWidgetCont box no-cursor">
         <strong className="cursor" style={{ width: 90 + '%' }}></strong>
         <CloseWidget id="imgWidget" />
-        <div>
+        <div className="imgWidCategory">
           <label htmlFor="category">Category : </label>
           <select
             name="category"
-            id="imgWidgetCategory"
+            id="imgWidgetCatSel"
             onChange={handleCategory}
           >
             {cat.map((item) => {
@@ -96,25 +97,40 @@ const ImgWidget = () => {
         {imgList && (
           <div className="imgWidgetWrap">
             <div className="imgPexelsInfo">
-              <a href={imgUrl.img.url} className="imgPexelsUser">
+              <a
+                href={imgUrl.img.url}
+                className="imgPexelsUser"
+                target={'_blank'}
+                rel="noreferrer"
+              >
                 by {imgUrl.img.photographer}
               </a>
 
-              <br />
-              <a href="https://www.pexels.com">photos provided by Pexels</a>
+              {/* <br />
+              <a href="https://www.pexels.com">photos provided by Pexels</a> */}
+              <a
+                href={imgUrl.img.src.original}
+                download="nature"
+                className="imgWidgetDown"
+                target={'_blank'}
+                rel="noreferrer"
+              >
+                <DownloadIcon fontSize="small" />
+              </a>
             </div>
 
             <img src={imgUrl.img.src.medium} alt={imgUrl.img.alt} />
           </div>
         )}
 
-        <div className="imgWidgetControls">
-          <button className="imgNextBtn" onClick={() => handleNext(imgUrl.id)}>
-            <ArrowForwardIosOutlinedIcon fontSize="large" />
-          </button>
-          <button className="imgPrevBtn" onClick={() => handlePrev(imgUrl.id)}>
-            <ArrowBackIosNewOutlinedIcon fontSize="large" />
-          </button>
+        <button className="imgNextBtn" onClick={() => handleNext(imgUrl.id)}>
+          <ArrowForwardIosOutlinedIcon fontSize="large" />
+        </button>
+        <button className="imgPrevBtn" onClick={() => handlePrev(imgUrl.id)}>
+          <ArrowBackIosNewOutlinedIcon fontSize="large" />
+        </button>
+
+        {/* <div className="imgWidgetControls">
           <button
             className="imgRefBtn"
             onClick={() => handleRefresh()}
@@ -122,7 +138,7 @@ const ImgWidget = () => {
           >
             Refresh
           </button>
-        </div>
+        </div> */}
       </div>
     </Draggable>
   );
